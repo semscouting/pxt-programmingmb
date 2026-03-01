18eedc3359f12c36b7664ee68416e2404200d2e5
Simon <Simon.Mastick@scouting.org>
2026-03-01 12:11:06 -0700

    Buzzer
    Still no tone

--- outputs.ts
+++ outputs.ts
@@ -6,9 +6,6 @@ namespace programmingmb {
         Off = 0
     }
 
-    let _buzzerPin: AnalogPin = AnalogPin.P0
-    let _buzzerIsOn = false
-
     //% block="set LED on %pin to %state"
     //% group="Outputs" weight=90
     export function ledSet(pin: DigitalPin, state: OnOff): void {
@@ -21,26 +18,15 @@ namespace programmingmb {
     //% block="set buzzer on %pin to %state"
     //% group="Outputs" weight=85
     export function buzzerSet(pin: DigitalPin, state: OnOff): void {
-        const analogPin = <AnalogPin><number>pin
-
-        // Reconfigure pitch pin only when it changes.
-        if (_buzzerPin != analogPin) {
-            _buzzerPin = analogPin
-            pins.analogSetPitchPin(_buzzerPin)
-            _buzzerIsOn = false
-        }
+        const aPin = <AnalogPin><number>pin
 
         if (state == OnOff.On) {
-            // Start tone only on OFF->ON transition to avoid choppy retriggering.
-            if (!_buzzerIsOn) {
-                music.ringTone(1000)
-                _buzzerIsOn = true
-            }
+            // PWM full-scale ON (works with many active buzzer modules)
+            pins.analogWritePin(aPin, 1023)
         } else {
-            if (_buzzerIsOn) {
-                music.stopAllSounds()
-                _buzzerIsOn = false
-            }
+            // PWM OFF
+            pins.analogWritePin(aPin, 0)
+            // Also force digital low to quiet some boards/modules
             pins.digitalWritePin(pin, 0)
         }
     }
