0a3b022895db9216696e68fb679ab87dac7d9faa
Simon <Simon.Mastick@scouting.org>
2026-03-01 10:25:40 -0700

    Update main.ts
    Changed color of the blocks.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 /**
  * Programming Merit Badge extension for micro:bit.
  */
-//% color="#0078D7" icon="\uf11b" block="Programming MB"
+//% color="#243E2C" icon="\uf11b" block="Programming MB"
 namespace programmingmb {
 }
